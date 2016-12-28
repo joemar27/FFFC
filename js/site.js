0e1e1de80a3fc1854a8b37441d2b1392064b7829
Joseph Hernandez <josephmar.hernandez@gmail.com>
2016-12-28 12:19:07 -0600

    WIP: Changed indent to 4 spaces

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 //  Move this to another page, create a "view" page for pages, and look into
 //  requirejs.
 var breadcrumb = function(){
-  var pagename = location.pathname;
-  this.parse_pagename = function(){
-    //regex for pagename.
-  }
-  this.compile_page_name = function(){
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(pagename);
-    node.appendChild(textnode);
-    document.getElementById("page_crumb").appendChild(node);
-  }
+    var pagename = location.pathname;
+    this.parse_pagename = function(){
+        //regex for pagename.
+    }
+    this.compile_page_name = function(){
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(pagename);
+        node.appendChild(textnode);
+        document.getElementById("page_crumb").appendChild(node);
+    }
 }
 
 var page_breadcrumb = new breadcrumb();
