d54c58ee8682d7bafd58190b0019292f56ba3bd5
Joseph Hernandez <josephmar.hernandez@gmail.com>
2017-02-18 22:24:14 -0600

    fix indentation

--- page_templates/html_template.php
+++ page_templates/html_template.php
@@ -113,8 +113,8 @@ class LoginPage{
 
     public function login_html_head(){
         $css_info = "
-        <link rel='stylesheet' type='text/css' href='css/auth.css'>
-        <script src='js/auth.js'></script>
+            <link rel='stylesheet' type='text/css' href='css/auth.css'>
+            <script src='js/auth.js'></script>
         ";
         return $css_info;
     }
